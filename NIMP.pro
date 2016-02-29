ba300a33ed4646243e722c48e5310cdbce4affff
GenaBitu <genabitu@gmail.com>
2016-02-29 15:00:12 +0100

    Mouse events

--- NIMP.pro
+++ NIMP.pro
@@ -12,7 +12,7 @@ TARGET = NIMP
 
 CONFIG += c++11
 
-QMAKE_CXXFLAGS += -std=c++11
+QMAKE_CXXFLAGS += -std=c++11 -Wnon-virtual-dtor -Winit-self -Wredundant-decls -Wcast-align -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -std=c++11 -Wfatal-errors -Wextra -Wall
 
 SOURCES += src/Main.cpp \
     src/Splitter.cpp \
--- src/Expander.cpp
+++ src/Expander.cpp
@@ -1,6 +1,6 @@
 #include "include/Expander.hpp"
 
-Expander::Expander(QString filename, SplitWidget* parent) : QLabel(parent), pixmap{new QPixmap{filename}}
+Expander::Expander(QString filename, SplitWidget* parent) : QLabel(parent), pressed{false}, pixmap{new QPixmap{filename}}
 {
     *pixmap = pixmap->scaledToHeight(32, Qt::FastTransformation);
     setPixmap(*pixmap);
@@ -11,4 +11,18 @@ Expander::Expander(QString filename, SplitWidget* parent) : QLabel(parent), pixm
 void Expander::reposition()
 {
     move(parentWidget()->width() - width(), 0);
+}
+
+void Expander::mousePressEvent(QMouseEvent* event)
+{
+    if(event->x() >= event->y())
+    {
+        pressed = true;
+    }
+}
+
+void Expander::mouseReleaseEvent(QMouseEvent* event)
+{
+    if(pressed and event->button() == Qt::LeftButton)
+    std::cerr << "Release at: " << event->x() << ", " << event->y() << std::endl;
 }
