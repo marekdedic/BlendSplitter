c4404a96f3e853b6fb1672510dea0366e45582cd
GenaBitu <genabitu@gmail.com>
2016-05-03 18:52:33 +0200

    Basic SwitchingWidget class, no special behaviour yet

--- include/RegistryItem.hpp
+++ include/RegistryItem.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#ifdef BLENDSPLITTER_LIBRARY
+    #define BLENDSPLITTER_EXPORT Q_DECL_EXPORT
+#else
+    #define BLENDSPLITTER_EXPORT Q_DECL_IMPORT
+#endif
+
 #include <QLabel>
 
-class RegistryItem
+class BLENDSPLITTER_EXPORT RegistryItem
 {
 public:
-    RegistryItem(QString name = "Default", QWidget* (*widget) () = []()->QWidget* {return new QLabel{"Inserted widget"};});
+    RegistryItem(QString name = "Default", QWidget* (*widget) () = []()->QWidget* {return new QLabel{"Inserted widget from Registry"};});
     QString name;
     QWidget* (*widget) ();
 };
