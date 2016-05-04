9f1d0ea38fcd880e4a106d3383642c912faa1433
Marek Dědič <developer@dedic.eu>
2016-05-04 13:05:36 +0200

    Proper SplitterCombo size

--- src/BlendSplitter/SwitchingBar.cpp
+++ src/BlendSplitter/SwitchingBar.cpp
@@ -1,15 +1,17 @@
 #include "include/BlendSplitter/SwitchingBar.hpp"
 
 #include "include/BlendSplitter/SplitterCombo.hpp"
+#include "include/BlendSplitter/Expander.hpp"
 
 int SwitchingBar::size = 48;
 
 SwitchingBar::SwitchingBar(QWidget* parent) : QWidget(parent)
 {
     QHBoxLayout* layout{new QHBoxLayout{}};
-    layout->addWidget(new SplitterCombo{});
-    layout->setMargin(0);
+    layout->setContentsMargins(Expander::size * 3 / 4, 0, 0, 0);
     setLayout(layout);
     setMinimumHeight(size);
     setMaximumHeight(size);
+    layout->addWidget(new SplitterCombo{});
+    layout->addStretch();
 }
