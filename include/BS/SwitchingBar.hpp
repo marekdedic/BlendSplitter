e49f60e875a8c04a7558b4320459bc0faab050fc
Marek Dědič <developer@dedic.eu>
2016-10-01 21:51:40 +0200

    Documented all public members of the public API

--- include/BS/SwitchingBar.hpp
+++ include/BS/SwitchingBar.hpp
@@ -13,12 +13,27 @@
 class SwitchingWidget;
 class SwitchingCombo;
 
+/** \brief A menu bar which is always found on the bottom of SwitchingWidget
+ *
+ * This menu bar is similar to the built-in QMenuBar, but can also contain plain QWidgets. The first item on the left is always a combo box for selecting which widget should be displayed in the SwitchingWidget.
+ */
 class BLENDSPLITTER_EXPORT SwitchingBar : public QWidget
 {
     Q_OBJECT
 public:
-    static int size;
+    static int size; /**< Height of the SwitchingBar. A static property. */
+    /** \brief Add a QMenu
+     *
+     * This function adds a QMenu to the very right of the SwitchingBar. The menu is wrapped in an invisible QMenuBar.
+     * \param menu A pointer to the QMenu to be added
+     * \return \todo Remove this.
+     */
     QAction* addMenu(QMenu* menu);
+    /** \brief Add a QWidget
+     *
+     * This function adds a QWidget to the very right of the SwitchingBar. The widget is placed in a QHBoxLayout.
+     * @param widget A pointer to the QWidget to be added
+     */
     void addWidget(QWidget* widget);
 protected:
     friend SwitchingWidget;
