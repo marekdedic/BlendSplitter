87c4ee998d52d122c0308f9ddd0adab59244fb84
GenaBitu <genabitu@gmail.com>
2016-10-03 08:10:52 +0200

    Added mainpage, better descriptions

--- search/all_1.js
+++ search/all_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['blendsplitter',['BlendSplitter',['../class_blend_splitter.html',1,'BlendSplitter'],['../class_blend_splitter.html#a8a305d92da45200aa9cc3e7160124cc3',1,'BlendSplitter::BlendSplitter()']]]
+  ['blendsplitter',['BlendSplitter',['../class_blend_splitter.html',1,'BlendSplitter'],['../class_blend_splitter.html#a8a305d92da45200aa9cc3e7160124cc3',1,'BlendSplitter::BlendSplitter()']]],
+  ['blendsplitter',['BlendSplitter',['../index.html',1,'']]]
 ];
